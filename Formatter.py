cd68d2f047c95ce2599045def4391e02fc53e09c
ritwik15416 <64138168+ritwik15416@users.noreply.github.com>
2020-07-23 17:46:47 +0530

    Update and rename Arithmetic Formatter.py to Formatter.py

--- Formatter.py
+++ Formatter.py
@@ -1,5 +1,5 @@
-def solve(problems,choice=False):
-    if len(problems) > 5:
+def solve(problems,choice=False):           
+    if len(problems) > 5:                # Checking for number of problems
         print("Error, Too many problems.")
     else:
         first = []
@@ -13,27 +13,27 @@ def solve(problems,choice=False):
             print(s_dash)
         for x in problems:
             new = x.split(" ")
-            if new[0].isdigit() == False or new[2].isdigit() == False:
+            if new[0].isdigit() == False or new[2].isdigit() == False:   # Checking if operands are digits only or not
                 print("Error: Numbers must only contain digits.")
                 exit = True
                 break
-            if len(new[0]) > 4 or len(new[2]) > 4:
+            if len(new[0]) > 4 or len(new[2]) > 4:            # Checking if operands' length is more than 4 digits or not
                 print("Error: Numbers cannot be more than four digits.")
                 exit = True
                 break
-            if (("+" not in new) and ("-" not in new)):
+            if (("+" not in new) and ("-" not in new)):            # Checking for operators
                 print("Error: Operator must be '+' or '-'.")
                 exit = True
                 break
                 
             else:
-                result.append(eval(x))
+                result.append(eval(x))   # Calculate results of problems
                 first.append(new[0])
                 second.append(new[1] + new[2])
                 dash.append("-----")
-        if(exit == False):
+        if(exit == False):           
             result = map(str,result)
-            s_first = ("\t").join(first)
+            s_first = ("\t").join(first)           
             s_second = ("\t").join(second)
             s_dash = ("\t").join(dash)
             s_result = ("\t").join(result)
